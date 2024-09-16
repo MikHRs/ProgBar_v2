a77bf7ed692a0e73ba5d4c9fc3939b17b705ef5d
MikHRs <michele.rais@edu.unifi.it>
2024-09-16 16:32:40 +0200

    first commit HO CREATO LA CONFIGURAZIONE OBSERVER ed anche il metodo load per simulare un caricamnto.Per fare questo metodo ho voluto utilizzare un thread, che ho gia studiato a sistemi operativi con java.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QProgressBar>
+#include <QLabel>
+#include <QPushButton>
+#include <thread>
+#include <chrono>
+#include "subject.h"
+class ProgressbarBarObservers : public Observer{
+private:
+    QProgressBar* progressBar;
+};
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
