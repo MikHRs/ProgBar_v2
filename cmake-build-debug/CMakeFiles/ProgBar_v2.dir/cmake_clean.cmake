7885066622fcb17960456d37a8528f1e6e54ce3f
MikHRs <michele.rais@edu.unifi.it>
2024-11-07 15:33:48 +0100

    Aggiornato ConcreteSubject per gestire internamente la lista di file da caricare, invece di riceverla tramite il metodo `load()`. La classe ora rappresenta il modello dei dati, con metodi per aggiungere e caricare i file. Rimosso il passaggio del vettore di file nella funzione `load()`.

---
+++ cmake-build-debug/CMakeFiles/ProgBar_v2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgBar_v2.dir/main.cpp.o"
+  "CMakeFiles/ProgBar_v2.dir/main.cpp.o.d"
+  "ProgBar_v2"
+  "ProgBar_v2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgBar_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
